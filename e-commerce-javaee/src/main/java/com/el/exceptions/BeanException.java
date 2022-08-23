0462fcf8fb341ae0282b47b71edbfd480c5063de
el sedy ba <elsedy000@gmail.com>
2022-08-23 21:50:29 +0000

    classes d'exception

---
+++ e-commerce-javaee/src/main/java/com/el/exceptions/BeanException.java
@@ -0,0 +1,7 @@
+package com.el.exceptions;
+
+// class d'exception liés aux javabeans 
+public class BeanException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ e-commerce-javaee/src/main/java/com/el/exceptions/DaoException.java
@@ -0,0 +1,7 @@
+package com.el.exceptions;
+
+// class d'exception liés a la base de donnée
+public class DaoException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+}
