4607b8d6a49a73b6100d9b47f27f61b0fdf643db
el sedy ba <elsedy000@gmail.com>
2022-08-30 17:41:45 +0000

    pour ecrire le fichier dans notre disque

--- e-commerce-javaee/src/main/java/com/el/beans/Image.java
+++ e-commerce-javaee/src/main/java/com/el/beans/Image.java
@@ -1,5 +1,11 @@
 package com.el.beans;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import jakarta.servlet.http.Part;
 
 public class Image {
@@ -39,5 +45,30 @@ public class Image {
 			}
 		}
 		return null;
+	}
+
+	// pour ecrire le fichier dans notre disque ou dans notre serveur si c'est deployer
+	public void ecrireFichier(Part part, String nomFichier, String chemin, int tailleTampon) throws IOException {
+		BufferedInputStream entree = null;
+		BufferedOutputStream sortie = null;
+		try {
+			entree = new BufferedInputStream(part.getInputStream(), tailleTampon);
+			sortie = new BufferedOutputStream(new FileOutputStream(new File(chemin + nomFichier)), tailleTampon);
+
+			byte[] tampon = new byte[tailleTampon];
+			int longueur;
+			while ((longueur = entree.read(tampon)) > 0) {
+				sortie.write(tampon, 0, longueur);
+			}
+		} finally {
+			try {
+				sortie.close();
+			} catch (IOException ignore) {
+			}
+			try {
+				entree.close();
+			} catch (IOException ignore) {
+			}
+		}
 	}
 }
