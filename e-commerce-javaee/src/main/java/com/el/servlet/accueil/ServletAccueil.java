6f7682c846f267f3949553e2e1b6fd1ac5d82886
el sedy ba <elsedy000@gmail.com>
2022-09-06 17:35:44 +0000

    creation de la servlet pour l'accueil

---
+++ e-commerce-javaee/src/main/java/com/el/servlet/accueil/ServletAccueil.java
@@ -0,0 +1,28 @@
+package com.el.servlet.accueil;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ServletAccueil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ServletAccueil() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/accueil.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
