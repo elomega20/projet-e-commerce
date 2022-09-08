cac79669ab4b3dfa25356a3fb4763dc60b95d496
el sedy ba <elsedy000@gmail.com>
2022-09-08 17:13:52 +0000

    contient les script jquery

---
+++ e-commerce-javaee/src/main/webapp/js/script.js
@@ -0,0 +1,37 @@
+
+$(function() {
+    // coloration orange/blanc sur le bouton panier
+	$("#id-div-panier").mouseenter(function() {
+		$("#id-a-panier").css("color", "#F4672c");
+	});
+	$("#id-div-panier").mouseleave(function() {
+		$("#id-a-panier").css("color", "black");
+	});
+	
+	// coloration orange/blanc sur le bouton compte
+	$("#btn-color").mouseenter(function() {
+		$("#btn-compte").css("color", "#F4672c");
+	});
+	$("#btn-color").mouseleave(function() {
+		$("#btn-compte").css("color", "black");
+	});
+	
+	// coloration orange/blanc sur le bouton aide
+	$("#id-div-aide").mouseenter(function() {
+		$("#id-btn-aide").css("color", "#F4672c");
+	});
+	$("#id-div-aide").mouseleave(function() {
+		$("#id-btn-aide").css("color", "black");
+	});
+	
+	// coloration orange/blanc sur le bouton rechercher
+	$("#id-btn-rechercher").mouseenter(function() {
+		$(this).css({"color" : "white","background-color" : "#F4672c"});
+	});
+	$("#id-btn-rechercher").mouseleave(function() {
+		$(this).css({"color" : "black","background-color" : "white"});
+	});
+	
+});
+
+
